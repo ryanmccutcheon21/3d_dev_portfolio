584eb8b460717a71f0b09a0c0570658ff0fc89cb
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-03-07 13:42:24 -0600

    Fix SectionWrapper misspell error

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 import 'react-vertical-timeline-component/style.min.css'
 import { styles } from '../styles'
 import { experiences } from '../constants'
-import { SectionWrapper } from '../hoc/SectionWrapper'
+import { SectionWrapper } from '../hoc'
 import { textVariant } from '../utils/motion'
 
 const ExperienceCard = ({ experience }) => (
