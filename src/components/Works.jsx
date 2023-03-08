b94dfb75e483f55f302271b8c8ef98d2f2fed2fc
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-03-08 01:23:07 -0600

    Fix grammar

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -53,7 +53,7 @@ const Works = () => {
       </motion.div>
       <div className='w-full flex'>
         <motion.p variants={fadeIn('', '', 0.1, 1)} className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-          The following projects showcase my skills and experience. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
+          The following projects showcase my skills and experience. Each project is briefly described with links to code repositories and live demos. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
         </motion.p>
       </div>
       <div className='mt-20 flex flex-wrap gap-7'>
